f7c49b4419fe189867608c9febc339c71e084508
joemoon-0 <moonjose@gmail.com>
2024-01-20 14:20:31 -0600

    from general set up, pre-web strip down

---
+++ nvim/lua/moon/core/keymaps.lua
@@ -0,0 +1,20 @@
+local key = vim.keymap
+
+vim.g.mapleader = " "
+key.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Use 'jk' to exit insert mode
+key.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+-- Page movement
+key.set("n", "<C-d>", "<C-d>zz")
+key.set("n", "<C-u>", "<C-u>zz")
+
+-- Bufferline keymaps
+key.set("n", "<C-t>", "<cmd>:tab split<CR>")
+key.set("n", "<C-h>", "<cmd>BufferLineCyclePrev<CR>")
+key.set("n", "<C-l>", "<cmd>BufferLineCycleNext<CR>")
+
+-- LSP Clang - toggle between header and implementation
+key.set("n", "<F3>", "<cmd>ClangdSwitchSourceHeader<CR>")
+key.set("n", "<F4>", "<cmd>lua open_hpp()<CR>")
